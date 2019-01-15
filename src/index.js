e22ee2331214484a1e8fa5de2ac9944f3768518f
PifagorRZ <zheka.rachkovan@gmail.com>
2019-01-15 17:13:48 +0200

    template page 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,9 +13,3 @@ ReactDOM.render(<Codeditor />, document.getElementById('editor'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-=======
-import {render} from 'react-dom';
-import App from "./components/App.js";
-
-render(<App/>, document.getElementById("root"));
->>>>>>> c048b3d433bb32659ce33844b711d2b786ccc2c0
