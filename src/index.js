18dba3c38c1e3fc5ca1decf3d22c96052960271d
PifagorRZ <zheka.rachkovan@gmail.com>
2019-01-15 17:31:48 +0200

    button

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import Codeditor from './codeEditor';
 import * as serviceWorker from './serviceWorker';
 
+const console = document.querySelector('#terminal'),
+      debug = document.getElementById('debug');
 
 
 ReactDOM.render(<Codeditor />, document.getElementById('editor'));
+
+debug.addEventListener('click', (e) => {
+     console.log("click");
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
