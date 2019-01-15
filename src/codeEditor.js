bb779328eb86bc503e5cfe607d9819243a269ca6
PifagorRZ <zheka.rachkovan@gmail.com>
2019-01-15 17:07:22 +0200

    template of site

---
+++ src/codeEditor.js
@@ -0,0 +1,28 @@
+import ReactAce from 'react-ace-editor';
+import React, { Component } from 'react';
+
+class CodeEditor extends Component {
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(newValue, e) {
+    console.log(newValue, e);
+
+    const editor = this.ace.editor; // The editor object is from Ace's API
+    console.log(editor.getValue()); // Outputs the value of the editor
+  }
+  render() {
+    return (
+      <ReactAce
+        mode="javascript"
+        theme="dracula"
+        onChange={this.onChange}
+        style={{ height: '100%', fontSize: '20px', position: 'static'} }
+        ref={instance => { this.ace = instance; }} // Let's put things into scope
+      />
+    );
+  }
+}
+
+export default CodeEditor;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
+<<<<<<< HEAD
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Codeditor from './codeEditor';
+import * as serviceWorker from './serviceWorker';
+
+
+
+ReactDOM.render(<Codeditor />, document.getElementById('editor'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+=======
 import {render} from 'react-dom';
 import App from "./components/App.js";
 
 render(<App/>, document.getElementById("root"));
+>>>>>>> c048b3d433bb32659ce33844b711d2b786ccc2c0
