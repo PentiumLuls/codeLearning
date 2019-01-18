a37953d74897c2a3c90940bbf7b2ce4d48997343
socthecat <socthecat@gmail.com>
2019-01-18 10:24:49 +0200

    ricardo milos ustrajivaet kamasutru

---
+++ src/components/CodeEditor/HellRules.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HellRules extends Component {
+
+    render() {
+        return (
+            <div class="hell-rules">
+                <p>ZHOPA</p>
+            </div>
+        );
+    }
+}
+export default HellRules;
--- src/components/Leftpanel/Header.js
+++ src/components/Leftpanel/Header.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  
+
+    constructor() {
+        super();
+        this.click = () => {
+            this.props.func();
+        }
+        this.click2 = () => {
+            this.props.func2();
+        }
+    }
+
     render() {
       return (
             <header className="userinfo">
                         <div className="avatar"><img alt="avatar" src="https://pbs.twimg.com/media/DYzVb6jX4AEuvrJ.jpg"/></div>
                         <div className="userbuttons">
-                            <span className="userbutton"><p>Knopka1</p></span>
-                            <span className="userbutton active"><p>Knopka2</p></span>
+                            <span className="userbutton" onClick={this.click}><p>Zhurnal</p></span>
+                            <span className="userbutton" onClick={this.click2}><p>Editor</p></span>
                         </div>
                     </header>
       );
     }
   }
   export default Header;
+
