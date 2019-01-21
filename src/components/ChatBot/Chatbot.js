513036f6a5857a7dfbb52fdd1fa90dd028c91c3b
Maks <pentiumluls@gmail.com>
2019-01-21 14:25:33 +0200

    dialogs

--- src/components/ChatBot/Chatbot.js
+++ src/components/ChatBot/Chatbot.js
@@ -1,31 +1,75 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {dialogs} from "../../plot/dialogs";
 
 export default class Chatbot extends Component {
-    state={visible:false}
-    showChat(){
-     const state = this.state;
-     state.visible = !state.visible;
-     this.setState({state})
+
+    constructor() {
+        super();
+
+        this.state = {
+            visible: false,
+            replics: [],
+        }
     }
 
-getDialogs() {
+    writeReplics = (replica, name, key) => {
+        /*let x = 0;
+        let interval = setInterval(() => {
+            let replic = `${name}:~$ ${replica[x++]}`;
+            this.setState({
+                replics: [...this.state.replics, replic]
+            });
+            if (x >= replics.length) {
+                clearInterval(interval);
+                localStorage.button_run = key || 0;
+            }
+        }, 1000);
+*/
 
-}
+        let x = 0;
+        let interval = setInterval(() => {
+            let replic = `${name}:~$ ${replica[x++]}`;
+            this.setState({
+                replics: [...this.state.replics, replic]
+            });
+            if (x >= dialogs.length) {
+                clearInterval(interval);
+                localStorage.button_run = key || 0;
+            }
+        }, 1000);
+    };
 
-    render(){
-       {if(this.state.visible === true)
-        return(
-            <div className='chatbot' >
-            <buttun className='buttonchatclose' onClick={this.showChat.bind(this)} >Close</buttun>
-             <div className='dialogbox'>{this.getDialogs} </div>
-             <buttun className='chatbuttun'   >nextDialog</buttun> <buttun className='chatbuttun'  >getHint</buttun>
-            </div>
-            );
-            return(
-                <div  >
-                    <buttun className='buttonchatopen' onClick={this.showChat.bind(this)} >Chat</buttun>
+    showChat() {
+        const state = this.state;
+        state.visible = !state.visible;
+        this.setState({state})
+    }
+
+
+    getDialogs = () => {
+        return (
+            <ul className="chatDialogs">
+                {this.state.replics.map((r, i) => <li key={i}>{r}</li>)}
+            </ul>
+        )
+    };
+
+
+    render() {
+        {
+            if (this.state.visible === true)
+                return (
+                    <div className='chatbot'>
+                        <buttun className='buttonchatclose' onClick={this.showChat.bind(this)}>Close</buttun>
+                        <div className="dialogbox">{this.getDialogs.bind(this)}</div>
+                        <buttun className='chatbuttun'   >nextDialog</buttun> <buttun className='chatbuttun'  >getHint</buttun>
+                    </div>
+                );
+            return (
+                <div>
+                    <buttun className='buttonchatopen' onClick={this.showChat.bind(this)}>Chat</buttun>
                 </div>
-                )
+            )
         }
     }
 }
