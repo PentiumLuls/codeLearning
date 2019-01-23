67b0def83776ff129386be26f5d612f3f3521379
PifagorRZ <zheka.rachkovan@gmail.com>
2019-01-23 12:48:22 +0200

    Create quests for 1 stage

--- src/plot/script.js
+++ src/plot/script.js
@@ -1,26 +1,8 @@
-var cows = {
-    milk: 10
+
+
+var x = 2;
+for(let o in window){ 
+   if(window[o] === x){
+      console.log(0);
+   }
 }
-
-function milkACow(cow) {
-    let newcow = Object.assign({}, cow);
-    let cheeses = []
-    return function(int) {
-        if (int >= newcow.milk) {
-            cheeses.push(newcow.milk / 2)
-            newcow.milk = 0
-            return cheeses;
-        }
-    newcow.milk -= int
-    cheeses.push(int / 2)   
-    }
-}
-
-
-
-
-let milcthisCOW = milkACow(cows)
-
-console.log(milcthisCOW(4));
-console.log(milcthisCOW(4));
-console.log(milcthisCOW(4));
