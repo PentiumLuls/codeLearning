4f337574f3e329e9d3b3153a31a324af5f03e74e
Maks <pentiumluls@gmail.com>
2019-01-24 15:05:55 +0200

    cheats deleted

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -31,13 +31,6 @@ class Terminal extends Component {
 
 
     run = () => {
-
-        ////LOCAL VARS////
-        //TYPE THIS TO COMPLETE CURRENT LEVEL
-        const completePLZ = this.unlockQuest();
-
-        //////////////////
-
         try {
             const vm = require('vm');
             const codeToEvaluate = localStorage.getItem("code") + "\n" + this.props.testCode["code"]
