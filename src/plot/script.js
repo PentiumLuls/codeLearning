3294d66c7d43ef885659466b8280be62b50bb407
DoctorLapen <kostia1272@gmail.com>
2019-01-31 18:17:24 +0200

    stage 4 5 level

--- src/plot/script.js
+++ src/plot/script.js
@@ -1,13 +1,19 @@
-function disEquipWeapon(weapon){
-    return (hindu)=>{
-     
-       const  newHindu = {...hindu}
-       const find = newHindu.weapons.indexOf(weapon)
-         if( find !== -1){
-            newHindu.weapons.splice(find,1);
-         }
-         return newHindu;
+const persons =[{weapons:['sword']}, {weapons:['sword','stick']},{weapons:['stick']},{weapons:['sword']}];
+function noWeapon(weapon){
+      return (hindu)=>{
+          return (hindu.weapons.indexOf(weapon) === -1) ?  1 : 0 ;
       }
-     }
- 
-console.log(disEquipWeapon('sword')({weapons:['sword','stick']}))
+};
+
+const noSword = noWeapon('sword');
+
+const noStick = noWeapon('stick');
+function sum(a,b){
+   return a + b;
+}
+
+const result1 = persons.map(noSword).reduce(sum,0);
+
+const result2 = persons.map(noStick).reduce(sum,0);
+console.log(result1)
+console.log(result2)
