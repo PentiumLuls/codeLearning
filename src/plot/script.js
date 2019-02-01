7ee1329e3f97933553afa1f3bc054ab7af7a8c11
DoctorLapen <kostia1272@gmail.com>
2019-02-01 10:38:50 +0200

    fix stage 4

--- src/plot/script.js
+++ src/plot/script.js
@@ -1,19 +1,9 @@
-const persons =[{weapons:['sword']}, {weapons:['sword','stick']},{weapons:['stick']},{weapons:['sword']}];
-function noWeapon(weapon){
-      return (hindu)=>{
-          return (hindu.weapons.indexOf(weapon) === -1) ?  1 : 0 ;
-      }
-};
-
-const noSword = noWeapon('sword');
-
-const noStick = noWeapon('stick');
-function sum(a,b){
-   return a + b;
+function sumMoney(eatCost){
+    return (hinduAmount) => {
+        return(mouthAmount) => {
+             return eatCost * hinduAmount * mouthAmount;
+        }
+    }
 }
-
-const result1 = persons.map(noSword).reduce(sum,0);
-
-const result2 = persons.map(noStick).reduce(sum,0);
-console.log(result1)
-console.log(result2)
+const result = sumMoney(50)(100)(3);
+console.log(result);
