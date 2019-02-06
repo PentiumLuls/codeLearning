0b9c9892304a1e3bbef9502a3e6f28ecbf7aa940
socthecat <socthecat@gmail.com>
2019-02-06 11:21:45 +0200

    щось страшне і криве + аватарки

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -35,7 +35,7 @@ if (!localStorage['achievements']) {
     localStorage['achievements'] = JSON.stringify([{id: 0, status: -1}, {id: 1, status: -1}, {id: 2, status: -1},
         {id: 3, status: -1}, {id: 4, status: -1}, {id: 5, status: -1, time: 0}, {id: 6, status: -1, payload: 0},
         {id: 7, status: -1, earned: 0}, {id: 8, status: -1, spend: 0}, {id: 9, status: -1}, {id: 10, status: -1},
-        {id: 11, status: -1, times: 0}, {id: 12, status: -1}], {id: 13, status: -1}, {id: 14, status: -1});
+        {id: 11, status: -1, times: 0}, {id: 12, status: -1}, {id: 13, status: -1}, {id: 14, status: -1}]);
 }
 if (!localStorage['timeInGame']) {
     localStorage['timeInGame'] = JSON.stringify({hours: 0, minutes: 0, seconds: 0})
