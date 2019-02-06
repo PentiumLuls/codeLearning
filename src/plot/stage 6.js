e1e21261467f2a8157ee2b2a2af1636406f67bf6
DoctorLapen <kostia1272@gmail.com>
2019-02-06 18:12:13 +0200

    stage 6 4

--- src/plot/stage 6.js
+++ src/plot/stage 6.js
@@ -162,7 +162,56 @@ function averageRaiting(gang){
                             averageRaiting
                             )(persons)
  console.log(result)
- ///// quest 6 //////                         
+ ///// quest 6 //////  
+ const legions  = [{number:13,size:4,names:['Hiran','Jayesh','Ojas','Ranbir'], attack: 300, location:'Demon`s castle'},
+{number:26,size:4,names:['Arnav','Himmat','Mitul','Pranay'], attack: 366, location:'Demon`s castle'},
+{number:6,size:6,names:['Ojas','Faiyaz','Rohan','Tushar','Aarav','Madhup'], attack: 353, location:'Hell lake'}] ;
+const data = {number:13,day:'Friday',shield:false};
+const devil = {health :510};
+ 
+function isCastle(legion){
+    return  legion.location == 'Demon`s castle';
+}
+
+function getAttackScope(legion){
+  return legion.attack;
+}
+function sum(a,b){
+    return a + b;
+}
+function modDamage(data){
+    return (damage) =>{
+        const modNumber = (data.number == 13) ? 0.1 : 0;
+        const modDay = (data.day == 'Friday') ? 0.13 : 0;
+        const modShield = (data.shield == true) ? 0.4 : 0;
+        return damage *(1 - modNumber - modDay - modShield)
+    }
+}
+
+function attack(target){
+    return(damage) => {
+            return target.health - damage;
+    }
+}
+function IsWin(health){
+ return health <= 0;
+}
+
+function answer(bool){
+ return (bool) ? 'We win!!!' :' We lose!!!' 
+}
+const attackDevil = compose(
+                          Filter(isCastle),
+                          Map(getAttackScope),
+                          Reduce(sum,0),
+                          modDamage(data),
+                          attack(devil),
+                          IsWin,
+                          answer
+                          )
+console.log(attackDevil(legions))                          
+
+                       
 
 
 
