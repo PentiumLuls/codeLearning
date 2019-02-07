f80cab2c046b558f6419fff4ebbf7447c423d27a
DoctorLapen <kostia1272@gmail.com>
2019-02-07 16:12:54 +0200

    st 6 q1 fix

--- src/plot/quests/chapter_6/quests/quest_1.js
+++ src/plot/quests/chapter_6/quests/quest_1.js
@@ -4,15 +4,15 @@ export const quest1 =
         text: `LEFT PANEL TEXT`,
         regexps: [],
         regexpsNone: [/(for)/g, /(while)/g, /(var)/g, /(let)/g],
-        code: `
-        
-// найди максимальный периметр черного прямоугольника;
+        code: `// найди максимальный периметр черного прямоугольника
+
 const rectangles = [{color: 'red',width:4,height:4,},
 {color: 'black',width:2, height:2,},{color: 'white',width:6,height:9,},
 {color: 'black',width:3,height:3,},{color: 'black',width:4,height:4, },
 {color: 'black',width:5,height:5,},{color: 'white',width:4,height:2,},{color: 'red',width:5,height:5,}];
+
 const compose = (...fns) => (arg) => 
-  fns.reduce((composed, f) => f(composed),arg)
+  fns.reduce((composed, f) => f(composed),arg);
 
 
 function Filter(callback) {
@@ -29,12 +29,13 @@ function Map(callback) {
 
 function isBlack(rectangle) {
     return rectangle.color == 'black';
-};
+}
         
 function perimeter(rectangle) {
     return rectangle.width*2 + rectangle.height*2;
 }  
-const result        `,
+
+const result`,
         hints: [
             "используй функцию compose",
              "используй функциональные версии Filter,Map,Reduce в compose"
@@ -43,23 +44,41 @@ const result        `,
             code: ``,
 
 
-            answer: `const rectangles = [{color: 'red',width:4,height:4,},
-            {color: 'black',width:2, height:2,},{color: 'white',width:6,height:9,},
-            {color: 'black',width:3,height:3,},{color: 'black',width:4,height:4, },
-            {color: 'black',width:5,height:5,},{color: 'white',width:4,height:2,},{color: 'red',width:5,height:5,}];
+        answer: `// найди максимальный периметр черного прямоугольника;
+const rectangles = [{color: 'red',width:4,height:4,},
+{color: 'black',width:2, height:2,},{color: 'white',width:6,height:9,},
+{color: 'black',width:3,height:3,},{color: 'black',width:4,height:4, },
+{color: 'black',width:5,height:5,},{color: 'white',width:4,height:2,},{color: 'red',width:5,height:5,}];
+ 
+const compose = (...fns) => (arg) => 
+  fns.reduce((composed, f) => f(composed),arg);
+
+
+function Filter(callback) {
+    return (mass) => mass.filter(callback);
+}
+
+function Reduce(callback,...arg) {
+    return (mass) => mass.reduce(callback,...arg);
+}
+
+function Map(callback) {
+      return (mass) => mass.map(callback);
+}
+
+function isBlack(rectangle) {
+    return rectangle.color == 'black';
+}
             
-            function isBlack(rectangle) {
-                return rectangle.color == 'black';
-                };
+function perimeter(rectangle) {
+    return rectangle.width*2 + rectangle.height*2;
+}  
             
-            function perimeter(rectangle) {
-                    return rectangle.width*2 + rectangle.height*2;
-            }  
-            
-            const result = compose (
-                           Filter(isBlack),
-                           Map(perimeter),
-                           Reduce(Math.max)
-                           )(rectangles)`
+const result = compose (
+                    Filter(isBlack),
+                    Map(perimeter),
+                    Reduce(Math.max)
+                    )(rectangles);
+terminal.log(result);                  `
         }
     };
