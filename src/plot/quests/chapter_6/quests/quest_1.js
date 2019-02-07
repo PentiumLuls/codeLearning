fc6234ec53f2436b7319a5f63b39676cac97bec8
Maks <pentiumluls@gmail.com>
2019-02-07 17:54:45 +0200

    chapter 6 update 1-2 quests

--- src/plot/quests/chapter_6/quests/quest_1.js
+++ src/plot/quests/chapter_6/quests/quest_1.js
@@ -4,81 +4,108 @@ export const quest1 =
         text: `LEFT PANEL TEXT`,
         regexps: [],
         regexpsNone: [/(for)/g, /(while)/g, /(var)/g, /(let)/g],
-        code: `// найди максимальный периметр черного прямоугольника
+        code: `// найди максимальный периметр черного прямоугольника;
 
-const rectangles = [{color: 'red',width:4,height:4,},
-{color: 'black',width:2, height:2,},{color: 'white',width:6,height:9,},
-{color: 'black',width:3,height:3,},{color: 'black',width:4,height:4, },
-{color: 'black',width:5,height:5,},{color: 'white',width:4,height:2,},{color: 'red',width:5,height:5,}];
+const rectangles = [
+{color: 'black', width:2, height:2},
+{color: 'white', width:6, height:9},
+{color: 'black', width:3, height:3},
+{color: 'black', width:4, height:5}
+];
 
 const compose = (...fns) => (arg) => 
-  fns.reduce((composed, f) => f(composed),arg);
-
+  fns.reduce((composed, f) => f(composed), arg);
 
 function Filter(callback) {
     return (mass) => mass.filter(callback);
 }
 
 function Reduce(callback,...arg) {
-    return (mass) => mass.reduce(callback,...arg);
+    return (mass) => mass.reduce(callback, ...arg);
 }
 
 function Map(callback) {
-      return (mass) => mass.map(callback);
+    return (mass) => mass.map(callback);
 }
 
 function isBlack(rectangle) {
     return rectangle.color == 'black';
 }
-        
-function perimeter(rectangle) {
-    return rectangle.width*2 + rectangle.height*2;
-}  
 
-const result`,
+function perimeter(rectangle) {
+    return rectangle.width * 2 + rectangle.height * 2;
+}
+
+function max(a, b) {
+    return a > b ? a : b;
+}
+
+const result = //YOUR CODE
+terminal.log(result);`,
         hints: [
             "используй функцию compose",
              "используй функциональные версии Filter,Map,Reduce в compose"
         ],
         test: {
-            code: ``,
+            code: `describe("1", function() {
+
+               it("result should equal to max perimeter of black rectangles",function() {
+                  expect(result).toEqual(compose(
+                    Filter(isBlack),
+                    Map(perimeter),
+                    Reduce(max))
+                    (rectangles));
+               });
+
+            });
+                  const report = runSpecs();
+                        for (var i = 0; i < report.passed.length; i++) {
+                          reporterLog(report.descriptions[i], report.passed[i])
+                        }
+                        if (report.passed.indexOf(false) === -1) { true } else false;`,
 
 
         answer: `// найди максимальный периметр черного прямоугольника;
-const rectangles = [{color: 'red',width:4,height:4,},
-{color: 'black',width:2, height:2,},{color: 'white',width:6,height:9,},
-{color: 'black',width:3,height:3,},{color: 'black',width:4,height:4, },
-{color: 'black',width:5,height:5,},{color: 'white',width:4,height:2,},{color: 'red',width:5,height:5,}];
- 
-const compose = (...fns) => (arg) => 
-  fns.reduce((composed, f) => f(composed),arg);
 
+const rectangles = [
+{color: 'black', width:2, height:2},
+{color: 'white', width:6, height:9},
+{color: 'black', width:3, height:3},
+{color: 'black', width:4, height:5}
+];
+
+const compose = (...fns) => (arg) => 
+  fns.reduce((composed, f) => f(composed), arg);
 
 function Filter(callback) {
     return (mass) => mass.filter(callback);
 }
 
 function Reduce(callback,...arg) {
-    return (mass) => mass.reduce(callback,...arg);
+    return (mass) => mass.reduce(callback, ...arg);
 }
 
 function Map(callback) {
-      return (mass) => mass.map(callback);
+    return (mass) => mass.map(callback);
 }
 
 function isBlack(rectangle) {
     return rectangle.color == 'black';
 }
-            
+
 function perimeter(rectangle) {
-    return rectangle.width*2 + rectangle.height*2;
-}  
-            
-const result = compose (
+    return rectangle.width * 2 + rectangle.height * 2;
+}
+
+function max(a, b) {
+    return a > b ? a : b;
+}
+
+const result = compose(
                     Filter(isBlack),
                     Map(perimeter),
-                    Reduce(Math.max)
-                    )(rectangles);
-terminal.log(result);                  `
+                    Reduce(max))
+                    (rectangles);
+terminal.log(result);`
         }
     };
