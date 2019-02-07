02ca7bf8b81b25c599fddba9abe214888b196bef
PifagorRZ <zheka.rachkovan@gmail.com>
2019-02-07 09:28:58 +0200

    fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,7 +32,13 @@ class App extends Component {
         }, 1000);
 
         this.player = null
-
+        fetch('https://powerful-woodland-78740.herokuapp.com/users/sign-in', {
+            method: 'PUT'
+        }).then(response => response.json())
+        .then(data => {
+          console.log(data) // Prints result from `response.json()` in getRequest
+        })
+        .catch(error => console.error(error));
     }
 
     changeButtonState = () => {
