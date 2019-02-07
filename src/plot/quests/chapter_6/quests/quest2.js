6bf413a346f9a0ba8717cba6c606b1e052ed6ade
Maks <pentiumluls@gmail.com>
2019-02-07 17:59:23 +0200

    quest 2 chapter 6 test cases

--- src/plot/quests/chapter_6/quests/quest2.js
+++ src/plot/quests/chapter_6/quests/quest2.js
@@ -51,7 +51,23 @@ terminal.log(swordFactory(materials))`,
             "используй функциональные версии Filter,Reduce в compose",
         ],
         test: {
-            code: ``,
+            code: `const swordFactoryTEST = ToolBox.compose(
+                            ToolBox.Filter(isIron),
+                            ToolBox.Reduce(sumIron,0),
+                            makeSwords);
+            
+            describe("2", function() {
+
+               it("sword factory should return amount of swords depends on materials",function() {
+                  expect(swordFactory(materials)).toEqual(swordFactoryTEST(materials));
+               });
+
+            });
+                  const report = runSpecs();
+                        for (var i = 0; i < report.passed.length; i++) {
+                          reporterLog(report.descriptions[i], report.passed[i])
+                        }
+                        if (report.passed.indexOf(false) === -1) { true } else false;`,
 
 
             answer: `//Cделай мечи для солдат
